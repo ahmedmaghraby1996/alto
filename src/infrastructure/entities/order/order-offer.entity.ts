cb6c88c29ff472267f5034583af8550d529b304a
Ahmed <a.maghraby96@yahoo.com>
2025-08-12 10:54:21 +0300

    package type

--- src/infrastructure/entities/order/order-offer.entity.ts
+++ src/infrastructure/entities/order/order-offer.entity.ts
@@ -4,8 +4,8 @@ import { Order } from './order.entity';
 import { OfferStatus } from 'src/infrastructure/data/enums/order-status.enumt';
 @Entity('order_offers')
 export class OrderOffer extends AuditableEntity {
-  @Column({ precision: 10, scale: 2, default: 0 })
-  price: number;
+@Column({ type: 'decimal', precision: 10, scale: 2, default: 0 })
+price: number;
 
   @Column({ default: OfferStatus.PENDING, type: 'enum', enum: OfferStatus })
   status: OfferStatus;
